83f9c6ea0542b575fa23e0b17e6fa2443d7d0cdd
Alp <aakpinar@bu.edu>
2023-03-28 23:07:36 +0200

    Add the simplified datacard for VBF

--- vbf/scripts/make_cards.sh
+++ vbf/scripts/make_cards.sh
@@ -5,7 +5,7 @@ mkdir -p cards
 # Fill templates
 for YEAR in 2017 2018; do
     CARD=cards/card_vbf_${YEAR}.txt
-    cp ../../templates/vbf_template_pretty_withphotons.txt ${CARD}
+    cp ../../templates/vbf_template_simplified.txt ${CARD}
     sed -i "s|@YEAR|${YEAR}|g" ${CARD}
 
     if [ $YEAR -eq 2017 ]; then
@@ -27,9 +27,6 @@ for YEAR in 2017 2018; do
         sed -i "/prefir/d" ${CARD}
     fi
     sed -i "s|combined_model.root|../root/combined_model_vbf.root|g" ${CARD}
-    sed -i "s|vbf_qcd_nckw_ws_${YEAR}.root|../root/vbf_qcd_nckw_ws_${YEAR}.root|g" ${CARD}
-    sed -i "s|vbf_hf_estimate_ws_${YEAR}.root|../root/vbf_hf_estimate_ws_${YEAR}.root|g" ${CARD}
-    sed -i "s|@RATEPARAM|1.0|g" ${CARD}
     text2workspace.py ${CARD} --channel-masks
     python $CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py --all -f html ${CARD} > cards/systematics_${YEAR}.html
 done
