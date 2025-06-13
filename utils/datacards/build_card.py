ee12325e00bbb0858c17d79d7556d93552884b84
Hugues EVARD <hugues.evard@cern.ch>
2025-06-13 19:20:54 +0200

    Bit of cleaning. Re-formated bin names

--- utils/datacards/build_card.py
+++ utils/datacards/build_card.py
@@ -1,8 +1,7 @@
 import os
 import CombineHarvester.CombineTools.ch as ch
 
-# aux_path = "./root/combined_model_vbf.root"
-aux_path = os.path.join(str(os.getenv("CMSSW_BASE")), "src/auxiliaries/shapes/")
+aux_path = "root/combined_model_vbf.root"
 
 cb = ch.CombineHarvester()
 
@@ -14,7 +13,7 @@ eras = ["Run3"]  # TODO: pass as an argument
 ## Regions (common for all analyses)
 region_names = ["signal", "dimuon", "dielec", "singlemu", "singleel", "photon"]
 # put in the form (index, name) for CombineHarvester
-regions = [(i, f"{analysis}_{region}") for i, region in enumerate(region_names)]
+regions = [(i, f"{region}") for i, region in enumerate(region_names)]
 
 ## Processes (might depend on the analysis)
 processes = {
@@ -84,54 +83,26 @@ cb.AddProcesses(
 
 ## CRs
 for region_idx, region_name in regions[1:]:
+    proc_label = region_name.split("_")[-1]
     cb.AddProcesses(
         mass=["*"],
         analysis=[analysis],
         era=eras,
         channel=[analysis],
-        procs=processes[region_name.replace(f"{analysis}_", "")]["backgrounds"] + processes[region_name.replace(f"{analysis}_", "")]["models"],
+        procs=processes[proc_label]["backgrounds"] + processes[proc_label]["models"],
         bin=[(region_idx, region_name)],
         signal=False,
     )
 
 ### Systematics
 # WIP, Adapted from example in https://gitlab.cern.ch/cms-analysis/nps/common/datacard-tutorial#combine-tutorial-for-the-nps-workshop-2025
-# cb.AddSyst(
-#     target=cb,
-#     name="lumi_$ERA",
-#     type="lnN",
-#     valmap=ch.SystMap("era")(["2016"], 1.010)(["2017"], 1.020)(["2018"], 1.015)(["Run3"], 1.015),
-# )
-
-#   cb.cp()
-#       .AddSyst(cb,
-#         "CMS_scale_j_$ERA", "lnN", SystMap<era, bin_id, process>::init
-#         ({"8TeV"}, {1},     {"ggH"},        1.04)
-#         ({"8TeV"}, {1},     {"qqH"},        0.99)
-#         ({"8TeV"}, {2},     {"ggH"},        1.10)
-#         ({"8TeV"}, {2},     {"qqH"},        1.04)
-#         ({"8TeV"}, {2},     {"TT"},         1.05));
-
-# import pdb
-
-# pdb.set_trace()
-# test_list = [(["Run3"], [region_idx], processes[region_name.replace(f"{analysis}_", "")]["backgrounds"], 1.015) for region_idx, region_name in regions]
-test_tuple2 = tuple((["Run3"], [region_idx], processes[region_name.replace(f"{analysis}_", "")]["backgrounds"], 1.015) for region_idx, region_name in regions)
-# test_tuple = (
-#     (["Run3"], [0], processes["signal"]["backgrounds"], 1.015),
-#     (["Run3"], [1], processes["signal"]["backgrounds"], 1.015),
-#     (["Run3"], [2], processes["signal"]["backgrounds"], 1.015),
-#     (["Run3"], [3], processes["signal"]["backgrounds"], 1.015),
-#     (["Run3"], [4], processes["signal"]["backgrounds"], 1.015),
-#     (["Run3"], [5], processes["signal"]["backgrounds"], 1.015),
-# )
-
 lumi_map = ch.SystMap("era", "bin_id", "process")
 for region_idx, region_name in regions:
+    proc_label = region_name.split("_")[-1]
     lumi_map(
         ["Run3"],
         [region_idx],
-        processes[region_name.replace(f"{analysis}_", "")]["signals"] + processes[region_name.replace(f"{analysis}_", "")]["backgrounds"],
+        processes[proc_label]["signals"] + processes[proc_label]["backgrounds"],
         1.015,
     )
 
@@ -141,8 +112,14 @@ cb.AddSyst(
     type="lnN",
     valmap=lumi_map,
 )
-# valmap = (ch.SystMap("era")(["2016"], 1.010)(["2017"], 1.020)(["2018"], 1.015)(["Run3"], 1.015),)
 
-# Models should not get lumi uncertainty
+# Standardize bin names
+ch.SetStandardBinNames(cb, "$CHANNEL_$ERA_$BIN")
 
-cb.WriteDatacard("test.txt", "test.root")
+# Extract shapes
+# TODO: will require changes to the scripts building the workspace
+# cb.cp().signals().ExtractShapes(aux_path, "category_vbf_$ERA:$CHANNEL_$ERA_$BIN_$PROCESS", "category_vbf_$ERA:$CHANNEL_$ERA_$BIN_$PROCESS_$SYSTEMATIC")
+# cb.cp().backgrounds().ExtractShapes(aux_path, "category_vbf_Run3/$BIN/$PROCESS", "$BIN/$PROCESS_$SYSTEMATIC")
+
+# cb.WriteDatacard("test.txt", "test.root")
+cb.WriteDatacard("test.txt")
